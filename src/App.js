2bf3c604398eaef195d5bb8fc3dbbc4083d8123b
Shishupal Kumar <shishupalkr2595@gmail.com>
2021-09-27 19:50:26 +0530

    Add todo with react-redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { connect } from "react-redux";
 
-function App() {
+function App(props) {
+
+  function handleTodo(event) {
+    if (event.keyCode === 13 && event.target.value) {
+      props.dispatch({ type: "add", todo: event.target.value });
+      event.target.value = "";
+    }
+  }
+  function handleRemove({ target }) {
+    let { id } = target.dataset;
+    props.dispatch({ type: "remove", id: id });
+  }
+
+  function handleCompleted({ target }) {
+    let { id, completed } = target.dataset;
+    props.dispatch({ type: "toggle", isCompleted: completed === "false" ? true : false, id: id })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <h1 className="text-center text-6xl font-bold mb-10 text-indigo-900">Todo List</h1>
+    <div className="bg-indigo-300 p-8 flex flex-col items-center mt-12 w-1/2 mx-auto rounded shadow-lg">
+      <input type="text" placeholder="What needs to  be done?" className="w-5/6 p-2 rounded-md shadow-lg" onKeyUp={handleTodo} />
+      <div className="w-full flex justify-center mt-10">
+        <ul className="">
+          {
+            props.state && props.state.map((elm, index) => {
+              return <li key={index} className="flex justify-between w-96 bg-indigo-200 my-3 items-center text-2xl p-3 rounded shadow-lg">
+                <input type="checkbox" onClick={handleCompleted} data-id={index} data-completed={elm.isCompleted} className="text-2xl" checked={elm.isCompleted ? true : false} />
+                <h2 className={elm.isCompleted ? "capitalize line-through" : "capitalize"}>{elm.todo}</h2>
+                <span className="cursor-pointer" onClick={handleRemove} data-id={index} >❌</span>
+              </li>
+            })
+          }
+        </ul>
+      </div>
     </div>
+    </>
   );
 }
+function mapStateToProps(state) {
+  return {
+    state: [...state]
+  }
+}
 
-export default App;
+export default connect(mapStateToProps)(App);
